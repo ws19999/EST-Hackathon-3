715538a932e7b30b345ae38b39e033664ecc6fcc
Woosung <127220113+ws19999@users.noreply.github.com>
2024-06-14 11:14:10 +0000

    .

--- first.py
+++ first.py
@@ -17,19 +17,26 @@ hide_sidebar()
 
 
 # 이미지 URL
-#url = ''
+logourl = 'https://cdn.pixabay.com/photo/2020/08/05/13/27/eco-5465461_640.png'
 
 # 이미지 다운로드
-#response = requests.get(url)
-#img = Image.open(BytesIO(response.content))
+response = requests.get(logourl)
+logoimg = Image.open(BytesIO(response.content))
+logoimg = logoimg.resize((200, 200))
+# 이미지 URL
+imgurl = 'https://images.unsplash.com/photo-1596902973163-061c44f4f34c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
+# 이미지 다운로드
+response = requests.get(imgurl)
+img = Image.open(BytesIO(response.content))
+img = img.resize((500,500))
 
 # 로그인 기능
 def first():
     # 이미지 표시
-    st.image('https://cdn.pixabay.com/photo/2020/08/05/13/27/eco-5465461_640.png',caption='로고자리?')
+    st.image(logoimg,caption='로고자리?')
     st.title("sample app")
-    st.image('https://images.unsplash.com/photo-1545169128-53e46e195d46?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',caption='애니메이션이나 기타 이미지자리')
+    st.image(img,caption='애니메이션이나 기타 이미지자리')
     st.page_link("pages/login.py", label="로그인")
     st.page_link("pages/join.py", label="회원가입")
 
