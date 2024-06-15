e260ec04dd048ad0214cd28d33e56118328ed195
Woosung <127220113+ws19999@users.noreply.github.com>
2024-06-15 15:22:27 +0000

    .

--- pages/gptpractice.py
+++ pages/gptpractice.py
@@ -2,6 +2,8 @@ import mysql.connector
 import streamlit as st
 from openai import OpenAI
 from st_chat_message import message
+import random
+temp=0
 def hide_sidebar():
     st.markdown("""
     <style>
@@ -41,19 +43,17 @@ def load_chat_history():
     rows = cursor.fetchall()
     
     if len(rows) > 50:
-        # 오래된 데이터의 timestamp 가져오기
+    # 삭제할 데이터의 타임스탬프를 가져오기
         cursor.execute(f"""
-            SELECT timestamp FROM (
-                SELECT timestamp FROM {table_name}
-                ORDER BY timestamp ASC
-                LIMIT 50 OFFSET 50
-            ) AS subquery
+        SELECT timestamp FROM {table_name}
+        ORDER BY timestamp DESC
+        LIMIT 1 OFFSET 49
         """)
-        timestamps_to_delete = cursor.fetchall()
+        timestamp_to_keep = cursor.fetchone()
 
         # 오래된 데이터 삭제
-        for (timestamp,) in timestamps_to_delete:
-            cursor.execute(f"DELETE FROM {table_name} WHERE timestamp = %s", (timestamp,))
+        if timestamp_to_keep:
+            cursor.execute(f"DELETE FROM {table_name} WHERE timestamp < %s", (timestamp_to_keep[0],))
             conn.commit()
 
         # 삭제 후 남은 데이터 다시 가져오기
@@ -61,6 +61,7 @@ def load_chat_history():
         rows = cursor.fetchall()
 
 
+    
     conn.close()
     return [{"role": "user", "content": user_message} if bot_response=='' else {"role": "assistant", "content": bot_response} for i, (user_message, bot_response, _) in enumerate(rows)]
 
@@ -73,12 +74,13 @@ st.title("마음들이")
 if "messages" not in st.session_state:
     st.session_state["messages"] = load_chat_history()
     if st.session_state['messages']==[]:
-        st.session_state["messages"].append({"role": "assistant", "content": "안녕? 만나서 반가워 "})
-    #st.session_state.messages.append({"role": "system", "content": "You are a friendly and polite assistant. Always respond in a kind and helpful manner. Encourage the user to explore self-discovery and learn about themselves. Ask engaging questions that prompt self-reflection and personal insights."})
-    st.session_state.messages.append({"role": "system", "content": "You are a friendly and polite assistant. Always respond in a kind and helpful manner. Ask questions that help to understand the user's personality traits, such as those used in MBTI or Big Five personality assessments."})
+        st.session_state["messages"].append({"role": "assistant", "content": "오늘 당신의 기분은 어떠신가요?"})
+    st.session_state.messages.append({"role": "system", "content": "You are a friendly and polite assistant. Always respond in a kind and helpful manner. Encourage the user to explore self-discovery and learn about themselves. Ask engaging questions that prompt self-reflection and personal insights."})
 for msg in st.session_state.messages:
     if msg["role"]!="system":
-        message(msg["content"], is_user=msg["role"] == "user")
+        #num=random.randint(0, 10000)
+        message(msg["content"], is_user=msg["role"] == "user",key = f"{temp}")
+        temp+=1
 
 def extract_keywords(prompt):
     response = client.chat.completions.create(
@@ -102,13 +104,21 @@ if prompt := st.chat_input():
     # 중요한 단어 추출
     important_words = extract_keywords(prompt)
     save_chat(prompt, "", important_words)  # 빈 bot_response와 words로 저장
-    message(prompt, is_user=True)
+    #num=random.randint(0, 10000)
+    
+    message(prompt, is_user=True,key = f"{temp}")
+    
     
     response = client.chat.completions.create(
         model="ft:gpt-3.5-turbo-1106:personal:ourchatbot:9a5TUFUx", 
         messages=st.session_state.messages
     )
     msg = response.choices[0].message.content
+    temp+=1
+    message(msg, is_user=False,key = f"temp")
     st.session_state.messages.append({"role": "assistant", "content": msg})
+    temp+=1
     save_chat('', msg,'')
-    message(msg, is_user=False)
+    #num=random.randint(0, 10000)
+    
+    
