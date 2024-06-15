7686aa7a9b6340f62678afbd1725f52bf8a83cac
Woosung <127220113+ws19999@users.noreply.github.com>
2024-06-15 05:08:55 +0000

    .

--- pages/gptpractice.py
+++ pages/gptpractice.py
@@ -40,7 +40,7 @@ def load_chat_history():
     cursor.execute(f"SELECT user_message, bot_response, timestamp FROM {table_name} ORDER BY timestamp ASC")
     rows = cursor.fetchall()
     conn.close()
-    return [{"role": "user", "content": user_message} if i % 2 == 0 else {"role": "assistant", "content": bot_response} for i, (user_message, bot_response, _) in enumerate(rows)]
+    return [{"role": "user", "content": user_message} if bot_response=='' else {"role": "assistant", "content": bot_response} for i, (user_message, bot_response, _) in enumerate(rows)]
 
 # openai API 키 인증
 openai_api_key = st.secrets["OPENAI_API_KEY"]
@@ -50,7 +50,8 @@ st.title("마음들이")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = load_chat_history()
-
+    #if st.session_state['messages'].empty():
+        #st.session_state.messages.append({"role": "system", "content": "You are a friendly and polite assistant. Always respond in a kind and helpful manner."})
 for msg in st.session_state.messages:
     message(msg["content"], is_user=msg["role"] == "user")
 
@@ -65,12 +66,12 @@ if prompt := st.chat_input():
     message(prompt, is_user=True)
     
     response = client.chat.completions.create(
-        model="gpt-4o", 
+        model="ft:gpt-3.5-turbo-1106:personal:ourchatbot:9a5TUFUx", 
         messages=st.session_state.messages
     )
     
     msg = response.choices[0].message.content
     words = " ".join(msg.split())  # 예시로 단어를 결합하여 words 필드를 채움
     st.session_state.messages.append({"role": "assistant", "content": msg})
-    save_chat(prompt, msg, words)
+    save_chat('', msg, words)
     message(msg, is_user=False)
